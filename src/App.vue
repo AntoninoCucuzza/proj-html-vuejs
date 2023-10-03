3539072d71e56ff91115531d550be4ee4dd165be
AntoninoCucuzza <cucuzza.antonino@hotmail.com>
2023-10-03 12:10:23 +0200

    add jumbo

--- src/App.vue
+++ src/App.vue
@@ -11,6 +11,15 @@ export default {
 
 <template>
   <AppHeader />
+  <div class="jumbo">
+  </div>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.jumbo {
+  height: 800px;
+  background-image: url(https://image.api.playstation.com/vulcan/ap/rnd/202206/0307/wIVJREWyBg8SyXvXYuBPG6je.png);
+  background-repeat: no-repeat;
+  background-size: cover;
+}
+</style>
