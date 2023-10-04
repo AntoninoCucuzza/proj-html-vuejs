b9b23ed5f7164940622f666521c4eb326b823990
AntoninoCucuzza <cucuzza.antonino@hotmail.com>
2023-10-04 17:58:41 +0200

    add hover to carousel miss img css sprite

--- src/components/main/carousel/SingleCard.vue
+++ src/components/main/carousel/SingleCard.vue
@@ -10,7 +10,12 @@ export default {
     ],
     data() {
         return {
-            vote: '<svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 576 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M316.9 18C311.6 7 300.4 0 288.1 0s-23.4 7-28.8 18L195 150.3 51.4 171.5c-12 1.8-22 10.2-25.7 21.7s-.7 24.2 7.9 32.7L137.8 329 113.2 474.7c-2 12 3 24.2 12.9 31.3s23 8 33.8 2.3l128.3-68.5 128.3 68.5c10.8 5.7 23.9 4.9 33.8-2.3s14.9-19.3 12.9-31.3L438.5 329 542.7 225.9c8.6-8.5 11.7-21.2 7.9-32.7s-13.7-19.9-25.7-21.7L381.2 150.3 316.9 18z"/></svg>'
+            vote: '<svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 576 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M316.9 18C311.6 7 300.4 0 288.1 0s-23.4 7-28.8 18L195 150.3 51.4 171.5c-12 1.8-22 10.2-25.7 21.7s-.7 24.2 7.9 32.7L137.8 329 113.2 474.7c-2 12 3 24.2 12.9 31.3s23 8 33.8 2.3l128.3-68.5 128.3 68.5c10.8 5.7 23.9 4.9 33.8-2.3s14.9-19.3 12.9-31.3L438.5 329 542.7 225.9c8.6-8.5 11.7-21.2 7.9-32.7s-13.7-19.9-25.7-21.7L381.2 150.3 316.9 18z"/></svg>',
+
+            bag: '<svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 448 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M160 112c0-35.3 28.7-64 64-64s64 28.7 64 64v48H160V112zm-48 48H48c-26.5 0-48 21.5-48 48V416c0 53 43 96 96 96H352c53 0 96-43 96-96V208c0-26.5-21.5-48-48-48H336V112C336 50.1 285.9 0 224 0S112 50.1 112 112v48zm24 48a24 24 0 1 1 0 48 24 24 0 1 1 0-48zm152 24a24 24 0 1 1 48 0 24 24 0 1 1 -48 0z"/></svg>',
+            heart: '<svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M47.6 300.4L228.3 469.1c7.5 7 17.4 10.9 27.7 10.9s20.2-3.9 27.7-10.9L464.4 300.4c30.4-28.3 47.6-68 47.6-109.5v-5.8c0-69.9-50.5-129.5-119.4-141C347 36.5 300.6 51.4 268 84L256 96 244 84c-32.6-32.6-79-47.5-124.6-39.9C50.5 55.6 0 115.2 0 185.1v5.8c0 41.5 17.2 81.2 47.6 109.5z"/></svg>',
+            big: '<svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M200 32H56C42.7 32 32 42.7 32 56V200c0 9.7 5.8 18.5 14.8 22.2s19.3 1.7 26.2-5.2l40-40 79 79-79 79L73 295c-6.9-6.9-17.2-8.9-26.2-5.2S32 302.3 32 312V456c0 13.3 10.7 24 24 24H200c9.7 0 18.5-5.8 22.2-14.8s1.7-19.3-5.2-26.2l-40-40 79-79 79 79-40 40c-6.9 6.9-8.9 17.2-5.2 26.2s12.5 14.8 22.2 14.8H456c13.3 0 24-10.7 24-24V312c0-9.7-5.8-18.5-14.8-22.2s-19.3-1.7-26.2 5.2l-40 40-79-79 79-79 40 40c6.9 6.9 17.2 8.9 26.2 5.2s14.8-12.5 14.8-22.2V56c0-13.3-10.7-24-24-24H312c-9.7 0-18.5 5.8-22.2 14.8s-1.7 19.3 5.2 26.2l40 40-79 79-79-79 40-40c6.9-6.9 8.9-17.2 5.2-26.2S209.7 32 200 32z"/></svg>',
+            eyes: '<svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 576 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M288 32c-80.8 0-145.5 36.8-192.6 80.6C48.6 156 17.3 208 2.5 243.7c-3.3 7.9-3.3 16.7 0 24.6C17.3 304 48.6 356 95.4 399.4C142.5 443.2 207.2 480 288 480s145.5-36.8 192.6-80.6c46.8-43.5 78.1-95.4 93-131.1c3.3-7.9 3.3-16.7 0-24.6c-14.9-35.7-46.2-87.7-93-131.1C433.5 68.8 368.8 32 288 32zM144 256a144 144 0 1 1 288 0 144 144 0 1 1 -288 0zm144-64c0 35.3-28.7 64-64 64c-7.1 0-13.9-1.2-20.3-3.3c-5.5-1.8-11.9 1.6-11.7 7.4c.3 6.9 1.3 13.8 3.2 20.7c13.7 51.2 66.4 81.6 117.6 67.9s81.6-66.4 67.9-117.6c-11.1-41.5-47.8-69.4-88.6-71.1c-5.8-.2-9.2 6.1-7.4 11.7c2.1 6.4 3.3 13.2 3.3 20.3z"/></svg>',
         }
     }
 }
@@ -20,16 +25,35 @@ export default {
     <div class="col-3">
         <div class="product">
             <img :src="img" alt="">
-            <div class="info">
+            <div class="info position-relative">
                 <div class="d-flex vote">
                     <div class="positive" v-for="asd in stars " v-html="vote"></div>
                     <div class="negative" v-for="asd in (5 - stars) " v-html="vote"></div>
                 </div>
-                <h6>{{ title }}</h6>
-                <p class="m-0">
+                <h6 class="px-2">{{ title }}</h6>
+                <p class="m-0 price pb-2 px-2">
                     <span> <span class="discount">{{ discount }}</span> {{ price }} </span>
                     <span> {{ pricetwo }}</span>
                 </p>
+                <div class="overlay-wrapper">
+                    <div class="overlay position-absolute d-none">
+                        <div class="row">
+                            <div class="col-3 pe-0">
+                                <div class="p-1 test text-center border-bottom-0 border-start-0" v-html="bag"></div>
+                            </div>
+                            <div class="col-3 p-0">
+                                <div class="p-1 test text-center border-bottom-0" v-html="heart"></div>
+                            </div>
+                            <div class="col-3 p-0">
+                                <div class="p-1 test text-center border-bottom-0" v-html="big"></div>
+                            </div>
+                            <div class="col-3 ps-0">
+                                <div class="p-1 test text-center border-bottom-0 border-end-0" v-html="eyes"></div>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
             </div>
         </div>
     </div>
@@ -39,23 +63,25 @@ export default {
 <style lang="scss" scoped>
 @use '../../../assets/scss/partials/variables' as *;
 
+
+
 .info {
-    padding: 0.5rem;
     background-color: $gh_violet_darker;
 
     p {
+
         span {
             color: $gh_warning;
 
             .discount {
-                color: gray;
+                color: $gh_gray;
                 text-decoration: line-through;
             }
         }
     }
 
     .vote {
-        padding: 0.25rem;
+        padding: 0.5rem;
 
         .positive {
             fill: $gh_warning ;
@@ -66,5 +92,28 @@ export default {
         }
 
     }
+
+    .overlay-wrapper {
+        position: relative;
+        padding: 0;
+        margin: 0;
+    }
+
+    &:hover .price {
+        visibility: hidden;
+    }
+
+
+    &:hover .overlay {
+        display: block !important;
+        bottom: 0;
+        left: 0;
+        width: 100%;
+
+        & .test {
+            border: 1px solid $gh_gray;
+            fill: white;
+        }
+    }
 }
-</style>
+</style>    
